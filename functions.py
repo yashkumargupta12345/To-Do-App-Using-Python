31abc0238338e6c54d3e7a7dd2d523f6bc36399f
yashkumargupta <guptayashyg45227@gmail.com>
2023-09-16 22:54:20 +0530

    Implemented the add feature in GUI

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """ Read a text file and return the list
      of to-do items """
     with open(filepath) as file_local:
@@ -6,7 +8,7 @@ def get_todos(filepath="todos.txt"):
         file_local.close()
         return todos_local
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text
     file"""
     with open(filepath, "w") as file_local:
--- gui.py
+++ gui.py
@@ -5,8 +5,22 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter to-do")
 add_button = sg.Button("Add")
 
-window = sg.Window("My to-do App",layout = [[label],[input_box, add_button]])
-window.read()
+window = sg.Window("My to-do App",
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
+
+while True:
+    event, values = window.read()
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
+
 window.close()
 
 
