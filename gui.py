3a2493625abc9935129e925a139a254c25a44d7c
yashkumargupta <guptayashyg45227@gmail.com>
2023-09-16 23:53:47 +0530

    Implemented the edit feature in GUI

--- gui.py
+++ gui.py
@@ -1,26 +1,47 @@
 import functions
 import PySimpleGUI as sg
 
+sg.theme("DarkTeal10")
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter to-do")
+input_box = sg.InputText(tooltip="Enter to-do", key='todo')
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 7])
+edit_button = sg.Button("Edit")
 
 window = sg.Window("My to-do App",
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button],
+                           [list_box, edit_button]],
                    font=("Helvetica", 20))
 
 while True:
     event, values = window.read()
+    print(event)
+    print(values)
     match event:
         case 'Add':
             todos = functions.get_todos()
             new_todo = values['todo'] + '\n'
             todos.append(new_todo)
             functions.write_todos(todos)
+            window[todos].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo'] + '\n'
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
-
+print("Bye!")
 window.close()
 
 
